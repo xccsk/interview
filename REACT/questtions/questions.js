7b1927db4f738c4528a1e375cafa6aac409f5b9c
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-05 21:13:33 +0100

    why use react instead of other frameworks

--- REACT/questtions/questions.js
+++ REACT/questtions/questions.js
@@ -29,3 +29,24 @@ High performance: React updates only those components that have changed, rather 
 //4. Q:What is the virtual DOM
 
 //A:DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree structure. Each branch of the tree ends in a node, and each node contains objects. React keeps a lightweight representation of the real DOM in the memory, and that is known as the virtual DOM. When the state of an object changes, the virtual DOM changes only that object in the real DOM, rather than updating all the objects. The following are some of the most frequently asked react interview questions.
+
+//5. Why use React instead of other frameworks
+
+/*
+Easy creation of dynamic applications: React makes it easier to create dynamic web applications because it provides less coding and provides more functionality, whereas, with JavaScript applications, code tends to get complex very quickly.
+
+Improved performance: React uses virtual DOM, which makes web applications perform faster. Virtual DOM compares its previous state and updates only those components in the real DOM, whose states have changed, rather than updating all the components — like conventional web applications.
+
+Reusable components: Components are the building blocks of any React application, and a single app usually consists of multiple components. These components have their own logic and controls, and they can be reused through the application, which, in turn, dramatically reduces the development time of an application.
+
+Unidirectional data flow: React follows a unidirectional data flow. This means that when designing a React app, we often nest child components within parent components. And since the data flows in a single direction, it becomes easier to debug errors and know where the problem occurs in an application at the moment.
+
+Dedicated tools for easy debugging: Facebook has released a chrome extension that we can use to debug React applications. This makes the process of debugging React to web applications faster and easier.
+*/
+
+
+//6. Q:What is the diffrence between the ES6 and ES5 standards
+
+//A: --
+
+
