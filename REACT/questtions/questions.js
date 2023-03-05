5c5525b727036ed837e3c76aa0acabfe2320e35c
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-05 21:04:55 +0100

    question 4

--- REACT/questtions/questions.js
+++ REACT/questtions/questions.js
@@ -16,3 +16,16 @@ High performance: React updates only those components that have changed, rather 
  */
 
 
+//2. Q:What is JSX
+
+//A:JSX is a syntax extension of JavaScript. It is used with React to describe what the user interface should look like. By using JSX, we can write HTML structures in the same file that contains JavaScript code.
+
+
+//3. Q:Can web browsers read JSX directly?
+
+//A:Web browsers cannot read JSX directly. This is because they are built to only read regular JS objects and JSX is not a regular JavaScript object. For a web browser to read a JSX file, the file needs to be transformed into a regular JavaScript object. For this, we use Babel
+
+
+//4. Q:What is the virtual DOM
+
+//A:DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree structure. Each branch of the tree ends in a node, and each node contains objects. React keeps a lightweight representation of the real DOM in the memory, and that is known as the virtual DOM. When the state of an object changes, the virtual DOM changes only that object in the real DOM, rather than updating all the objects. The following are some of the most frequently asked react interview questions.
