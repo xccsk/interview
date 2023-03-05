e529601647f7ddb189983cb8edd8b54b01ea7416
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-03-05 17:26:48 +0100

    JS interview questions

---
+++ JS/questions.js
@@ -0,0 +1,91 @@
+/*
+JS interview Questions 
+*/
+
+// 1. Q:Explain event delegation.
+// A:
+
+// 2. Q:Explain how this works in JavaScript.Can you give an example of one of the ways that working with this has changed in ES6?
+// A:
+
+//3. Q:Explain how prototypal inheritance works.
+
+//4. Q:What's the difference between a variable that is: null, undefined or undeclared?How would you go about checking for any of these states?
+
+//5. Q:What is a closure, and how/why would you use one?
+
+//6. Q:What language constructions do you use for iterating over object properties and array items?
+
+//7. Q:Can you describe the main difference between the Array.forEach() loop and Array.map() methods and why you would pick one versus the other?
+
+//8. Q:What's a typical use case for anonymous functions?
+
+//9. Q:What's the difference between host objects and native objects?
+
+//10. Q:Explain the difference between: function Person(){}, var person = Person(), and var person = new Person()?
+
+//11. Q:Explain the differences on the usage of foo between function foo() {} and var foo = function() {}
+
+//12. Q:Can you explain what Function.call and Function.apply do? What's the notable difference between the two?
+
+//13. Q:Explain Function.prototype.bind.
+
+//14. Q:What's the difference between feature detection, feature inference, and using the UA string?
+
+//15. Q:Explain "hoisting".
+
+//16. Q:Describe event bubbling.
+
+//17. Q:Describe event capturing.
+
+//18. Q:What's the difference between an "attribute" and a "property"?
+
+//19. Q:What are the pros and cons of extending built-in JavaScript objects?
+
+//20. Q:What is the difference between == and ===?
+
+//21. Q:Explain the same-origin policy with regards to JavaScript.
+
+//22. Q:Why is it called a Ternary operator, what does the word "Ternary" indicate?
+
+//23. Q:What is strict mode? What are some of the advantages/disadvantages of using it?
+
+//24. Q:What are some of the advantages/disadvantages of writing JavaScript code in a language that compiles to JavaScript?
+
+//25. Q:What tools and techniques do you use debugging JavaScript code?
+
+//26. Q:Explain the difference between mutable and immutable objects. What is an example of an immutable object in JavaScript? What are the pros and cons of immutability? How can you achieve immutability in your own code?
+
+//27. Q:Explain the difference between synchronous and asynchronous functions.
+
+//28. Q:What is event loop?
+
+//29. Q:What is the difference between call stack and task queue?
+
+//30. Q:What are the differences between variables created using let, var or const?
+
+//31. Q:What are the differences between ES6 class and ES5 function constructors?
+
+//32. Q:Can you offer a use case for the new arrow => function syntax? How does this new syntax differ from other functions?
+
+//33. Q:What advantage is there for using the arrow syntax for a method in a constructor?
+
+//34. Q:What is the definition of a higher-order function?
+
+//35. Q:Can you give an example for destructuring an object or an array?
+
+//36. Q:Can you give an example of generating a string with ES6 Template Literals?
+
+//37. Q:Can you give an example of a curry function and why this syntax offers an advantage?
+
+//38. Q:hat are the benefits of using spread syntax and how is it different from rest syntax?
+
+//39. Q:How can you share code between files?
+
+//40. Q:Why you might want to create static class members?
+
+//41. Q:What is the difference between while and do-while loops in JavaScript?
+
+//42. Q:What is a promise? Where and how would you use promise?
+
+//43. Q:Discuss how you might use Object Oriented Programming principles when coding with JavaScript.
