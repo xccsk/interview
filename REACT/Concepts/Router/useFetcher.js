c2a0d3c54b63d244fe46956a89bfd78aee9d28d5
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-13 19:36:04 +0200

    useFetcher

---
+++ REACT/Concepts/Router/useFetcher.js
@@ -0,0 +1,9 @@
+//fetcher
+
+/*
+this is only used when you dont want to navigate to the page where the loader belongs so you need to set the action property on the form and define the path on wich action this should be triggert. without loading that page. because with a normal action or loader you would be directed to the part of the page where thats happening
+
+when sending via fetcher you can also can get feedback lke sending with (action and loader) by destructering the fetcher object
+
+
+*/
