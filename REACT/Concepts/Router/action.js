2bf496f3838bdb78f3317dbf09306e4ee7d2cc35
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-13 19:36:37 +0200

    action

---
+++ REACT/Concepts/Router/action.js
@@ -0,0 +1,9 @@
+//Action 
+
+/*
+when using a form in a route you can use actions to get the input of the forms and send a http request for eg. and with that you need to do this with a react specific element the Form element
+
+u also have an object as that is passed as argument the {requests, params} object that u can use in your action form
+
+as the loader you can also get data from an action response with the {useActionData} hook that provides the response in json to you for example on onvalid form inputs
+*/
