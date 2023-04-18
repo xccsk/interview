849b4d2a8d5b0b2b6711af2a49cb03f14438ff69
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-18 22:39:46 +0200

    kind of tests

---
+++ REACT/Concepts/Unit Test/kindsoftest.js
@@ -0,0 +1,31 @@
+//what kinds of 
+
+/*
+Unit Tests
+
+test the individual building blocks and functions (components) in isolation
+
+Projects typiclly contain dozens or hundrests of unit tests
+
+its the most commen and importent kind of test
+*/
+
+/*
+Integration Tests
+
+Test the combination of mutiple building blocks
+
+Projects typically cotain a couple of integration tests
+
+also important, but unit tests are most focused on in most cases 
+*/
+
+/*
+End-to-End tests
+
+test complete scenarios in your app as the user would experience them
+
+Projects typically contain only few e2e tests
+
+important but can be done manually
+*/
