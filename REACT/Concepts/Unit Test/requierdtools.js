3b62394ed1df621057f28e63587d3bb7c6e4cc2b
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-20 09:47:17 +0200

    naming convention

--- REACT/Concepts/Unit Test/requierdtools.js
+++ REACT/Concepts/Unit Test/requierdtools.js
@@ -4,4 +4,7 @@
 most commen tool to run tests its jest
 
 and we need a tool to run our app while doing that and thats the react testng libary
+
+a commen convention is to name your files after the coponent you test means if you test App.js you need to name your app App.test.js
+
 */
