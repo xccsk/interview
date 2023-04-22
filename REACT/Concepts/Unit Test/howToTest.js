610fa80b8df74adc29865f335ef1e3a3ab24573b
Johannes Stahr <johannesstahrofficial@gmail.com>
2023-04-22 21:27:10 +0200

    testing in react apps

--- REACT/Concepts/Unit Test/howToTest.js
+++ REACT/Concepts/Unit Test/howToTest.js
@@ -1,1 +1,11 @@
-//Write the test as colse as possible to the componen
+//Write the test as colse as possible to the componen as possible
+
+//when components get bigger you may want to groupe these things and get them togehter you need to get the descript method and get two arguments to it 
+
+/*for one test you the test('descriptiton,()=>{
+here test function
+})*/
+
+/*
+when usng descript you can grupe test and just put all the test method in the descripe function
+*/
